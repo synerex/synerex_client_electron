28c88b6de213fa859f71e111201665ab5ab8c0c6
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-01-20 21:30:08 +0900

    add auto scroll

--- js/main.js
+++ js/main.js
@@ -5,22 +5,23 @@ const Terminal = require('terminal.js');
 
 let nodeTerm = null;
 let sxTerm = null;
+let nodeTermP, sxTermP;
 
 ipc.on('started', function(){
     console.log("Started!")
-    var cs = document.getElementById('nodeterm')
-    console.log('node:',cs)
+    nodeTermP = document.getElementById('nodeterm')
+    console.log('node:',nodeTermP)
     nodeTerm = new Terminal({columns:80,rows:200})
     nodeTerm.state.setMode('crlf', true);
-    nodeTerm.dom(cs)
+    nodeTerm.dom(nodeTermP)
     nodeTerm.write("")
 //    cs.appendChild(nodeTerm.html)
 
     sxTerm = new Terminal({columns:80,rows:200})
     sxTerm.state.setMode('crlf', true);
-    cs = document.getElementById('sxterm')
-    console.log("sx",cs)
-    sxTerm.dom(cs)
+    sxTermP = document.getElementById('sxterm')
+    console.log("sx",sxTermP)
+    sxTerm.dom(sxTermP)
     sxTerm.write("")
  //   cs.appendChild(sxTerm.html)
 
@@ -38,10 +39,22 @@ ipc.on('started', function(){
 
 ipc.on('nodelog', function(event, data){
     nodeTerm.write(data)
-    console.log('Got:'+data)
+    nodeTermP.scrollTo({
+        top: (nodeTerm.state.cursor.y-10) * 14.5,
+        left: 0,
+        behavior: 'smooth'
+    } )
+    console.log('Got:'+data, 'scrooll'+ nodeTerm.state.cursor.y*10)
+
 })
 ipc.on('sxlog', function(event, data){
     sxTerm.write(data)
+    sxTermP.scrollTo({
+        top: (sxTerm.state.cursor.y-10) * 14.5,
+        left: 0,
+        behavior: 'smooth'
+    } )
     console.log('Got:'+data)
+    console.log('SXscrooll'+ sxTerm.state.cursor.y*10)
 })
 
